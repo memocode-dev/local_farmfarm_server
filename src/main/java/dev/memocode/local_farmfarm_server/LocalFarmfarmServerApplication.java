4bcc2e6b3ee4941f06a448826ae6abf4a9d86c35
audgks5551 <audgsk5551@daum.net>
2024-06-22 08:55:22 +0900

    init

---
+++ src/main/java/dev/memocode/local_farmfarm_server/LocalFarmfarmServerApplication.java
@@ -0,0 +1,13 @@
+package dev.memocode.local_farmfarm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFarmfarmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalFarmfarmServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/memocode/local_farmfarm_server/LocalFarmfarmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.memocode.local_farmfarm_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFarmfarmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
